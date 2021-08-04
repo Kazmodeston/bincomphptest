b8302ebfe496d6bd90ff927c4951e8847af9bba2
Kazim Muhammad <kazmodeston1@gmail.com>
2021-08-04 21:43:40 +0100

    created logic for question one and two

---
+++ app/Models/AnnouncedPullingResult.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AnnouncedPullingResult extends Model
+{
+    use HasFactory;
+
+    protected $table = "announced_pu_results";
+}
