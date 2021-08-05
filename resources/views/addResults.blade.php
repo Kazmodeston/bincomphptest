a1570edef0c2861c22de82f422c50d1dc2f17224
Kazim Muhammad <kazmodeston1@gmail.com>
2021-08-05 03:00:25 +0100

    populated ward from LGA

--- resources/views/addResults.blade.php
+++ resources/views/addResults.blade.php
@@ -7,18 +7,59 @@
             <div class="row">
                 <div class="col-sm-4"></div>
                 <div class="col-sm-4">
-                    <form action="#" id="pulling_result">
+                    
+                    <form action="{{url('/add-result')}}" method="POST">
                         @csrf
-                        <div class="form-group">
-                            <select name="" id="select_pulling" class="form-control">
-                                <option value="">Select Pulling</option>
 
+                        <div class="form-group">
+                            <label for="LGA">LGA</label>
+                            <select class="form-control" name="lga" id="lga">
+                                <option value="">Choose LGA</option>
                                 @foreach ($results as $result)
                                     <option value="{{$result->lga_id}}">{{$result->lga_name}}</option>
                                 @endforeach
                             </select>
+                            <span class="error"></span>
                         </div>
+
+                        <div class="form-group">
+                            <label for="Ward">Ward</label>
+                            <select class="form-control" name="ward" id="ward">
+                                <option value="">Choose Ward</option>
+                            </select>
+                            <span class="error"></span>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="pullingUnit">Pulling Unit</label>
+                            <input type="text" class="form-control" id="pullingUnit" placeholder="enter pulling unit">
+                            <span class="error"></span>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="Ward">party</label>
+                            <select class="form-control" name="party" id="party">
+                                <option value="">Choose Ward</option>
+                            </select>
+                            <span class="error"></span>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="pullingUnit">Party Scores</label>
+                            <input type="text" class="form-control" id="score" placeholder="enter Party Scores">
+                            <span class="error"></span>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="pullingUnit">Entered by</label>
+                            <input type="text" class="form-control" id="enter_by" placeholder="enter Party Scores">
+                            <span class="error"></span>
+                        </div>
+
+        
+                        <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
+                    
                 </div>
                 <div class="col-sm-4"></div>
             </div>
@@ -41,40 +82,30 @@
 <script type="text/javascript">
     $(document).ready(function() {
         
-        $("#select_pulling" ).change(function() {
+        $("#lga" ).change(function() {
             const selector = $(this).val();
             
             $.ajax({
-                        url: "lga-pulling-result",
+                        url: "get-ward",
                         type: "GET", //send it through get method
                         data: { 
                            lga : selector
                         },
                         success: function(response) {
-                            if (response.length > 0) {
-                                $("#polling_data").empty();
-                                console.log(response)
+                            console.log(response)
+                            // return
+                            $("#ward").empty();
 
-                                if(response[0].summed_total === null)
-                                {
-                                    $("#polling_data").append("<tr class='delete'>" + "<td >" + "No result for this Local government" + "</td>" + "</tr>");
-                                }
-                                else
-                                {
-                                    
-                                    $("#polling_data").append(
-                                        "<tr>" + "<td>" + "Result Total" + "</td>" + "<td>" + response[0].summed_total + "</td>" + "</tr>");
-                        
-                                    const title = $("#select_pulling option:selected" ).text();
-                                    $("#polling_title" ).html(title);
+                            response.forEach(function(response, i){
+                                
+                                $("#ward").append(
+                                    "<option value=" + response.ward_id + ">" + response.ward_name + "</option>");
+                            })
 
-                                }
-
-                               
+                            if (response.length === 0) {
+                                $("#ward").empty();
                             }
-                            else{
-                                $("#polling_data").append("<tr class='delete'>" + "<td >" + "No result for this Local government" + "</td>" + "</tr>");
-                            }
+                            
 
                         },
                         error: function(xhr) {
